4c81f4517fdb093fcec94fc6ff3cb4b83cf61c10
VictoriaPal <59703217+VictoriaPal@users.noreply.github.com>
2020-01-27 18:42:30 -0500

    Add character controller script

---
+++ Platformer_Version_Control/Assets/Scripts/Player Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private string inputAxis = "move";
+    private string inputTurn = "turn";
+
+    public float turnRotation = 360;
+    public float speed = 10;
+
+    private void Update()
+    {
+        
+    }
+
+
+}
