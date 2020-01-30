8256c372aae6fc746504db98650d8af268642871
Dragona17 <57118324+Dragona17@users.noreply.github.com>
2020-01-30 13:14:56 -0500

    Added to game menu script
    Added the start and quit button functions

---
+++ Platformer_Version_Control/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void StartButton()
+    {
+        SceneManager.LoadScene("Playscene", LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    void QuitButton()
+    {
+        Application.Quit();
+    }
+}
