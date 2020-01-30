9a3fa550b06f70ea67a08092aa8bf35560786e97
LVLWON <45217028+LVLWON@users.noreply.github.com>
2020-01-29 20:16:58 -0500

    Implemented AI Patrolling
    Testing AI orbit around chosen point. Folder structure re-arranged as well.

--- Platformer_Version_Control/Assets/AI Scripts/SkuaPatrol.cs
+++ Platformer_Version_Control/Assets/AI Scripts/SkuaPatrol.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SkuaPatrol : StateMachineBehaviour
+
 {
+
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
