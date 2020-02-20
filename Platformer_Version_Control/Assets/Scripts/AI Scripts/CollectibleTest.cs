7a78fc244f76145b89ca0e9c1321d39f1a6a65bc
LVLWON <45217028+LVLWON@users.noreply.github.com>
2020-02-20 16:25:59 -0500

    Added Objective to Complete Loop
    Added a yellow cube for the player to touch, creating a goal for the player. Wrote the script to return to the main menu upon successfully touch it and securing the victory. Necessary to complete the gameplay loop.

---
+++ Platformer_Version_Control/Assets/Scripts/AI Scripts/CollectibleTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollectibleTest : MonoBehaviour
+{
+
+    //On collision with player
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        //If the object tagged "player" collides with the collectible, change scene to the main menu
+        if (collision.gameObject.tag == "Player")
+        {
+
+            //Load the menu scene
+            SceneManager.LoadScene("GameMenu", LoadSceneMode.Single);
+        }
+    }
+}
