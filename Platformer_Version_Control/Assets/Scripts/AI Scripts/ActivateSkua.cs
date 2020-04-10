efa5c4c8cd714ba9c5c7f8761082515de7012b5b
VictoriaPal <59703217+VictoriaPal@users.noreply.github.com>
2020-04-09 21:49:15 -0400

    Skua Triggers done

---
+++ Platformer_Version_Control/Assets/Scripts/AI Scripts/ActivateSkua.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateSkua : MonoBehaviour
+{
+
+    public GameObject Skua;
+    public GameObject triggerSpere;
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+
+            Skua.SetActive(true);
+
+            Destroy(triggerSpere);
+        }
+    }
+}
